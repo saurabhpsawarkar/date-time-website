0d6853c0efeb8f1cb5cf30f1d165c0f604e915d2
saurabh <saurabhsawarkar@gmail.com>
2025-06-20 12:56:14 +0530

    changes in code

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Install Node.js
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+sudo yum install -y nodejs
+
+# Initialize and install express
 cd /home/ec2-user/app
-npm install
+npm install express
